5949f60cc507efceb4b404cc8157729f08a4881c
Arzu Yildoz <ajyildiz@outlook.com>
2019-10-23 19:30:18 -0400

    added new packages

--- src/App.js
+++ src/App.js
@@ -5,24 +5,30 @@ import NavBar from './components/NavBar';
 import Search from './components/Search';
 import MyCollection from './components/MyCollection';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {CSSTransition, TransitionGroup} from 'react-transition-group';
 
 function App() {
   return (
     <MuiThemeProvider>
     <div className="App">
     <Router>
-      <Switch>
-        <Route>
-          <NavBar />
-          <Search path="/" exact component={ Search }/>
-        </Route>
-      </Switch>
-      <Switch>
-        <Route>
-          <NavBar />
-          <MyCollection path="/MyCollection" component={ MyCollection }/>
-        </Route>
-      </Switch>
+      <TransitionGroup>
+        <CSSTransition
+          timeout={300}
+          className="fade"
+        >
+        <Switch>
+          <Route>
+            <NavBar />
+            <Search path="/" exact component={ Search }/>
+          </Route>
+          <Route>
+            <NavBar />
+            <MyCollection path="/MyCollection" component={ MyCollection }/>
+          </Route>
+        </Switch>
+        </CSSTransition>
+      </TransitionGroup>
     </Router>
     </div>
     </MuiThemeProvider>
